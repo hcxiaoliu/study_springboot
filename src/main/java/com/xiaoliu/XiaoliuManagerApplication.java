5cd88f6ba13cddf6fea194c5afbb3e82cc6ab0f4
haxiaoliu <2637294447@qq.com>
2019-05-23 00:04:28 +0800

    连接了阿里云数据库和单机版redis

---
+++ src/main/java/com/xiaoliu/XiaoliuManagerApplication.java
@@ -0,0 +1,14 @@
+package com.xiaoliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoliuManagerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(XiaoliuManagerApplication.class, args);
+    }
+
+}
